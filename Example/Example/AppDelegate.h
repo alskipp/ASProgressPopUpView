a41898f4cbed2d60494aa9c57eeacf4822781b08
al_skipp <al_skipp@fastmail.fm>
2014-04-16 14:59:10 +0100

    Initial commit

---
+++ Example/Example/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by Alan Skipp on 16/04/2014.
+//  Copyright (c) 2014 Alan Skipp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/Example/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Example
+//
+//  Created by Alan Skipp on 16/04/2014.
+//  Copyright (c) 2014 Alan Skipp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
